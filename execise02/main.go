471f4ed486b1fba0a81099f0453a7d611186ea76
songrgg <songrgg0.0@gmail.com>
2020-05-02 07:07:47 -0700

    add golang namespace exercise PID & UTS

---
+++ execise02/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("/bin/bash")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Error running the exec.Command - %s\n", err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module namespace-demo
+
+go 1.14
+
+require github.com/docker/docker v1.13.1 // indirect
